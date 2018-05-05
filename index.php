3a57d7196c8ed0dd1e2c482e81d98a1b42d7fd85
hartuna <bartlomiejhartuna@gmail.com>
2018-05-05 17:13:38 +0200

    Pause and stop script

--- index.php
+++ index.php
@@ -9,35 +9,42 @@
 <body>
 	<div id="container">
 		<div class="skills">
+			<div class="diode"></div>
 			<h3>PHP</h3>
 			<div class="hidden">
-				<div class="status">0</div>
+				<div class="status">2</div>
 				<div class="miliseconds">0</div>
 				<div class="pause">0</div>
 			</div>
 			<div class="time">0.00 min</div>
-			<button>Start</button>
+			<button class="start">Start</button>
+			<button class="zero">Zeruj</button>
 		</div>
 		<div class="skills">
+			<div class="diode"></div>
 			<h3>JavaScript</h3>
 			<div class="hidden">
-				<div class="status">0</div>
+				<div class="status">2</div>
 				<div class="miliseconds">0</div>
 				<div class="pause">0</div>
 			</div>
 			<div class="time">0.00 min</div>
-			<button>Start</button>
+			<button class="start">Start</button>
+			<button class="zero">Zeruj</button>
 		</div>
 		<div class="skills">
+			<div class="diode"></div>
 			<h3>SQL</h3>
 			<div class="hidden">
-				<div class="status">0</div>
+				<div class="status">2</div>
 				<div class="miliseconds">0</div>
 				<div class="pause">0</div>
 			</div>
 			<div class="time">0.00 min</div>
-			<button>Start</button>
+			<button class="start">Start</button>
+			<button class="zero">Zeruj</button>
 		</div>
+		<p id="info">Przycisk "Zeruj" jest dostępny po zatrzymaniu licznika</p>
 	</div>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,39 +1,60 @@
 window.onload = function(){
-	var button = document.getElementsByTagName('button');
+	var zero = document.getElementsByClassName('zero');
+	var start = document.getElementsByClassName('start');
 	for(var i = 0; i < 3; i++){
 		let index = i;
-		button[i].addEventListener('click', function(){changeStatus(index)});
+		start[i].addEventListener('click', function(){changeStatus(index)});
+		zero[i].addEventListener('click', function(){clear(index)});
 	}
 	time();
 }
 function changeStatus(index){
 	var seconds = new Date();
 	var addTime = document.getElementsByClassName('time');	
-	var button = document.getElementsByTagName('button');
+	var zero = document.getElementsByClassName('zero');
 	var mseconds = document.getElementsByClassName('miliseconds');
+	var start = document.getElementsByClassName('start');
 	var status = document.getElementsByClassName('status');
 	var stop = document.getElementsByClassName('pause');
-	if(status[index].textContent == 0){
-		button[index].textContent = 'Pauza';
+	if(status[index].textContent == 0 || status[index].textContent == 2){
+		start[index].textContent = 'Pauza';
 		mseconds[index].textContent = seconds.getTime();
 		status[index].textContent = 1; 
+		zero[index].disabled = true;
 	}
 	else{
-		button[index].textContent = 'Start';
+		start[index].textContent = 'Start';
 		stop[index].textContent = parseFloat(addTime[index].textContent, 10);
 		mseconds[index].textContent = 0;
 		status[index].textContent = 0;
+		zero[index].disabled = false;
 	}
+}
+function clear(index){
+	var addTime = document.getElementsByClassName('time');
+	var status = document.getElementsByClassName('status');
+	var stop = document.getElementsByClassName('pause');
+	stop[index].textContent = 0.00;
+	addTime[index].textContent = '0.00 min';	
+	status[index].textContent = 2; 
 }
 function time(){
 	var seconds = new Date();
-	var status = document.getElementsByClassName('status');
-	var addTime = document.getElementsByClassName('time');	
+	var addTime = document.getElementsByClassName('time');
+	var diode = document.getElementsByClassName('diode');	
 	var mseconds = document.getElementsByClassName('miliseconds');
+	var status = document.getElementsByClassName('status');
 	var stop = document.getElementsByClassName('pause');
 	for(var i = 0; i < 3; i++){
 		if(status[i].textContent == 1){
 			addTime[i].textContent =  (parseFloat(stop[i].textContent, 10) + (seconds.getTime() - mseconds[i].textContent) / 60000).toFixed(2) + ' min';
+			diode[i].style.backgroundColor = '#0c0';
+		}
+		else if(status[i].textContent == 2){
+			diode[i].style.backgroundColor = '#c00';
+		}
+		else{
+			diode[i].style.backgroundColor = '#ca0';
 		}
 	}
 	setTimeout(time, 100);
