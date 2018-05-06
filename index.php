d4f92f50076604594379b2b3dd063aaa5bb99511
hartuna <bartlomiejhartuna@gmail.com>
2018-05-06 22:25:29 +0200

    Import script - continuation

--- index.php
+++ index.php
@@ -48,27 +48,38 @@ session_start();
 			<button class="zero">Zeruj</button>
 		</div>
 		<p id="info">Przycisk "Zeruj" jest dostępny po zatrzymaniu licznika</p>
-		<form action="/time-counter/import.php" method="post">
-			<input type="hidden" value="0.00" name="php">
-			<input type="hidden" value="0.00" name="javascript">
-			<input type="hidden" value="0.00" name="sql">
-			<input type="password" name="password">
-			<input type="submit" value="Import" name="send">
-			<?php
-			if(isset($_SESSION['statement'])){
-			?>
-			<p><?php echo $_SESSION['statement']; ?></p>
-			<?php
-			unset($_SESSION['statement']);	
-			}
-			else if(isset($_SESSION['error'])){
-			?>
-			<p><?php echo $_SESSION['error']; ?></p>
-			<?php	
-			unset($_SESSION['error']);
-			}
-			?>
-		</form>
+		<div id="form">
+			<div id="import">
+				<p>Importuj czas do bazy</p>
+				<button id="importTime"></button>
+			</div>
+			<form action="/time-counter/import.php" method="post">
+				<input type="hidden" value="0.00" name="php">
+				<input type="hidden" value="0.00" name="javascript">
+				<input type="hidden" value="0.00" name="sql">
+				<label for="password">Wprowadź hasło i zatwierdź</label>
+				<input id="password" type="password" name="password">
+				<input id="send" type="submit" value="" name="send">
+				<button id="close"></button>
+			</form>
+			<div id="statement">
+				<p id="result">
+				<?php
+				if(isset($_SESSION['statement'])){
+					echo $_SESSION['statement'];
+					unset($_SESSION['statement']);	
+				}
+				else if(isset($_SESSION['error'])){
+					echo $_SESSION['error'];
+					unset($_SESSION['error']);
+				}
+				else{
+					echo '';
+				}
+				?>	
+				</p>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 window.onload = function(){
+	var close = document.getElementById('close');
+	var importTime = document.getElementById('importTime');
+	var result = document.getElementById('result');
 	var start = document.getElementsByClassName('start');
 	var zero = document.getElementsByClassName('zero');
 	for(var i = 0; i < 3; i++){
@@ -6,6 +9,11 @@ window.onload = function(){
 		start[i].addEventListener('click', function(){changeStatus(index)});
 		zero[index].disabled = true;
 		zero[i].addEventListener('click', function(){clear(index)});
+	}
+	importTime.addEventListener('click', function(){showForm(0.00)});
+	close.addEventListener('click', closeForm);
+	if(result.textContent != ''){
+		showResult(1.00, 'show');
 	}
 	time();
 }
@@ -40,6 +48,51 @@ function clear(index){
 	addTime[index].textContent = '0.00 min';	
 	status[index].textContent = 2; 
 	zero[index].disabled = true;
+}
+function showForm(opacity){
+	var importForm = document.getElementById('import');
+	var form = document.getElementsByTagName('form');
+	opacity += 0.05;
+	if(opacity <= 1.00){
+		if(opacity == 0.05){
+			form[0].style.display = 'inline-block';
+		}
+		importForm.style.opacity = 1.00 - opacity;
+		form[0].style.opacity = opacity;
+		setTimeout(function(){showForm(opacity)}, 10);
+	}
+	else{
+		importForm.style.opacity = 0.00;
+		importForm.style.display = 'none';
+		form[0].style.opacity = 1.00;
+	}
+}
+function closeForm(opacity){
+	var importForm = document.getElementById('import');
+	var form = document.getElementsByTagName('form');
+	importForm.style.display = 'inline-block';
+	importForm.style.opacity = 1.00;		
+	form[0].style.opacity = 0.00;
+	form[0].style.display = 'none';
+}
+function showResult(opacity, direction){
+	var statement = document.getElementById('statement');
+	if(direction == 'show'){
+		statement.style.display = 'inline-block';
+		statement.style.opacity = 1.00;
+		direction = 'hide';
+		setTimeout(function(){showResult(opacity, direction)}, 1000);
+	}
+	else{
+		if(opacity >= 0.00){
+			opacity -= 0.02;
+			statement.style.opacity = opacity;
+			if(opacity < 0.02){
+				statement.style.display = 'none';
+			}
+			setTimeout(function(){showResult(opacity, direction)}, 10);
+		}
+	}
 }
 function time(){
 	var seconds = new Date();
